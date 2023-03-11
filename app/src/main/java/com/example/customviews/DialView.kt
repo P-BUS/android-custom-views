882b0faf212a402546a9114a5dd4b0952ac0d8d5
P-BUS <pavlo.buslowski@gmail.com>
2023-03-11 14:34:16 +0200

    added DialView a subclass of View

---
+++ app/src/main/java/com/example/customviews/DialView.kt
@@ -0,0 +1,22 @@
+package com.example.customviews
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+private enum class FunSpeed(val label: Int) {
+    OFF(R.string.fan_off),
+    LOW(R.string.fan_low),
+    MEDIUM(R.string.fan_medium),
+    HIGH(R.string.fan_high),
+}
+private const val RADIUS_OFFSET_LABEL = 30
+private const val RADIUS_OFFSET_INDICATOR = -35
+
+class DialView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+}
