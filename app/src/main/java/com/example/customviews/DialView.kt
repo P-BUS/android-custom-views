d901b0ee2c9b7557a81e829c77c5ef2d0015271c
P-BUS <pavlo.buslowski@gmail.com>
2023-03-12 20:18:41 +0200

    bind colors to circle

--- app/src/main/java/com/example/customviews/DialView.kt
+++ app/src/main/java/com/example/customviews/DialView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.withStyledAttributes
 import kotlin.math.min
 import kotlin.math.sin
 import kotlin.math.cos
@@ -41,9 +42,18 @@ class DialView @JvmOverloads constructor(
         textSize = 55.0F
         typeface = Typeface.create("", Typeface.BOLD)
     }
+    private var fanSpeedLowColor = 0
+    private var fanSpeedMediumColor = 0
+    private var fanSpeedHighColor = 0
 
     init {
         isClickable = true
+
+        context.withStyledAttributes(attrs, R.styleable.DialView) {
+            fanSpeedLowColor = getColor(R.styleable.DialView_fanColor1, 0)
+            fanSpeedMediumColor = getColor(R.styleable.DialView_fanColor2, 0)
+            fanSpeedHighColor = getColor(R.styleable.DialView_fanColor3, 0)
+        }
     }
 
     override fun performClick(): Boolean {
@@ -65,7 +75,12 @@ class DialView @JvmOverloads constructor(
         super.onDraw(canvas)
 
         // Set dial background color to green if selection not off.
-        paint.color = if (fanSpeed == FanSpeed.OFF) Color.GRAY else Color.GREEN
+        paint.color = when (fanSpeed) {
+            FanSpeed.OFF -> Color.GRAY
+            FanSpeed.LOW -> fanSpeedLowColor
+            FanSpeed.MEDIUM -> fanSpeedMediumColor
+            FanSpeed.HIGH -> fanSpeedHighColor
+        } as Int
 
         // Draw the dial
         canvas.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), radius, paint)
